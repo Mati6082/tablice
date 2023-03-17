ace50e37b6ec848fc887bbd3b45e5d57cba9da23
matpakla <matpakla@gmail.com>
2023-03-17 09:24:35 +0100

    kod

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,25 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import kotlin.reflect.typeOf
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    val tablica = arrayOf(1f,2f,3f)
+
+    for (el in tablica)
+        print("$el ")
+    println()
+
+    for (i in 0 .. tablica.size-1)
+        print("${tablica[i]} ")
+    println()
+
+    for (i in tablica.indices)
+        print("${tablica[i]} ")
+
+    var arrayList = ArrayList<Float>()
+
+    arrayList.add(5f)
+    arrayList.add(1.0f)
+
+    val list = listOf<Float>(1f,2f,3f)
+
+    val mlist = mutableListOf(3f,1f,5f,1)
 }
